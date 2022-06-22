5e7e7b3846930ba862bed53b984e687486c8c3ef
Umidjan <97349110+umidjandev@users.noreply.github.com>
2022-06-22 22:21:42 +0500

    Setup RecycleView

---
+++ app/src/main/java/com/najdimu/shoppinglist/other/ShoppingItemAdapter.kt
@@ -0,0 +1,49 @@
+package com.najdimu.shoppinglist.other
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.najdimu.shoppinglist.R
+import com.najdimu.shoppinglist.data.db.entities.ShoppingItem
+import com.najdimu.shoppinglist.ui.shoppinglist.ShoppingViewModel
+import kotlinx.android.synthetic.main.shopping_item.view.*
+
+class ShoppingItemAdapter(
+    var items: List<ShoppingItem>,
+    private val viewModel: ShoppingViewModel
+): RecyclerView.Adapter<ShoppingItemAdapter.ShoppingViewHolder>() {
+
+
+    inner class ShoppingViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShoppingViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.shopping_item, parent, false)
+        return ShoppingViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ShoppingViewHolder, position: Int) {
+        val curShoppingItem = items[position]
+
+        holder.itemView.tvName.text = curShoppingItem.name
+        holder.itemView.tvAmount.text = "${curShoppingItem.amount}"
+
+        holder.itemView.ivDelete.setOnClickListener {
+            viewModel.delete(curShoppingItem)
+        }
+
+        holder.itemView.ivPlus.setOnClickListener {
+            curShoppingItem.amount++
+            viewModel.upsert(curShoppingItem)
+        }
+
+        holder.itemView.ivMinus.setOnClickListener {
+            if (curShoppingItem.amount > 0) {
+                curShoppingItem.amount--
+                viewModel.upsert(curShoppingItem)
+            }
+        }
+    }
+
+    override fun getItemCount() = items.size
+}
